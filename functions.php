46b31a999b3fc4dd57dd197bb6478f9f88b2a4a1
Drew Hornbein <drew@goodgoodwork.io>
2020-01-31 11:40:59 -0700

    adds custom login url

--- functions.php
+++ functions.php
@@ -91,3 +91,28 @@ add_action( 'elementor/query/programming_past', function( $query ) {
     $post_slug = $post->post_name;
 	$query->set( 'tag', $post_slug );
 } );
+
+
+add_filter( 'login_url', 'bta_custom_login_url', 10, 3 );
+/**
+ * Filters the login URL.
+ *
+ * @since 2.8.0
+ * @since 4.2.0 The `$force_reauth` parameter was added.
+ *
+ * @param string $login_url    The login URL. Not HTML-encoded.
+ * @param string $redirect     The path to redirect to on login, if supplied.
+ * @param bool   $force_reauth Whether to force reauthorization, even if a cookie is present.
+ *
+ * @return string
+ */
+function bta_custom_login_url( $login_url, $redirect, $force_reauth ){
+    $login_url = site_url( '/login/', 'login' ); // @TODO make this dynamic...
+    if ( ! empty( $redirect ) ) {
+        $login_url = add_query_arg( 'redirect_to', urlencode( $redirect ), $login_url );
+    }
+    if ( $force_reauth ) {
+        $login_url = add_query_arg( 'reauth', '1', $login_url );
+    }
+    return $login_url;
+}
