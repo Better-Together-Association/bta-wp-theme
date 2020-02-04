382a8e464f2c255cb2fab956ddb857a3ab7c9e28
Drew Hornbein <drew@goodgoodwork.io>
2020-02-04 15:46:08 -0700

    changes required to relavitve url, whoops

--- functions.php
+++ functions.php
@@ -23,10 +23,10 @@ function hello_elementor_child_enqueue_scripts() {
 add_action( 'wp_enqueue_scripts', 'hello_elementor_child_enqueue_scripts' );
 
 // pull in shortcodes
-require get_stylesheet_directory_uri() . '/includes/bta-shortcodes.php';
+require 'includes/bta-shortcodes.php';
 
 // pull in custom Elementor Queries
-require get_stylesheet_directory_uri() . '/includes/bta-elementor-queries.php';
+require 'includes/bta-elementor-queries.php';
 
 // pull in custom filters
-require get_stylesheet_directory_uri() . '/includes/bta-filters.php';
+require 'includes/bta-filters.php';
